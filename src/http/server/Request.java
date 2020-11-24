68cdf2c88c9d49dc5d69b2f2bdfb03d43bd4b1f7
Gheorghe Tutunaru <tutunaru.gheorghe111@gmail.com>
2020-11-24 12:24:42 +0100

    Update request with parsing of params in URI

--- src/http/server/Request.java
+++ src/http/server/Request.java
@@ -2,6 +2,9 @@ package http.server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,10 +15,11 @@ public class Request {
     public String uri;
     public String version;
     public Map<String,String> headers;
+    public Map<String, String > params;
     public String body;
     public boolean wellFormed;
 
-    public Request(String method, String uri, String version,
+    public Request(String method, String uri, String version, Map<String,String> params,
                    Map<String,String> headers, String body, boolean wellFormed){
         this.method = method;
         this.uri = uri;
@@ -23,6 +27,7 @@ public class Request {
         this.headers = headers;
         this.body = body;
         this.wellFormed = wellFormed;
+        this.params = params;
     }
 
     public static Request parse(InputStream is) throws IOException{
@@ -36,6 +41,13 @@ public class Request {
         String method = parts[0];
         String uri = parts[1];
         String version = parts[2];
+        int indexParam = uri.indexOf('?');
+        Map<String,String> params = new HashMap<String, String>();;
+        if (indexParam != -1){
+            URL url = new URL(uri);
+            uri = uri.substring(0,indexParam);
+            params = splitQuery(url);
+        }
         Map<String, String> headers = new HashMap<String, String>();
         String headerLine;
         while (true){
@@ -58,7 +70,18 @@ public class Request {
             body = scanner.next();
         }
         System.err.println("Request created");
-        return new Request(method, uri, version, headers, body, wellFormed);
+        return new Request(method, uri, version, params, headers, body, wellFormed);
 
+    }
+
+    public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
+        Map<String, String> query_pairs = new HashMap<String, String>();
+        String query = url.getQuery();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+        }
+        return query_pairs;
     }
 }
